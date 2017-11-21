95996a6de86a23dff7fec7054109a2f2165457a2
Ekansh pali <ekanshpali@gmail.com>
2017-11-21 22:46:15 +0530

    Update prob12.cpp

--- prob12.cpp
+++ prob12.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 vector<int> triangle;
 int listfactors(long long x)
 {
-    int counter=2;
-    for (int i=2;i<=x/2;i++)
-    {
-        if (x%i==0)
-            counter++;
-        while (x%i==0 && x!=1)
-        {
-            x/=i;
-        }
-    }
+    int counter=0;
+   for (int i=1;i<=sqrt(x);i++)
+   {
+       if (x%i==0)
+       {
+           if (x/i==i)
+           {
+               counter++;
+           }
+           else
+           {
+               counter+=2;
+           }
+       }
+   }
     return counter;
 }
 
@@ -23,8 +29,7 @@ int main()
     for (int i=2;;i++)
     {
         triangle.push_back(i+triangle[i-2]);
-        cout<<listfactors(triangle[i-1])<<" ";
-       if (listfactors(triangle[i-1])>=5)
+       if (listfactors(triangle[i-1])>=500)
        {
            cout<<triangle[i-1];
            break;
